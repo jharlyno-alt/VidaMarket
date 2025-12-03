04bd6237c19689a77bb41a905acf13bc73b7ffe2
jharlyno-alt <jharlyno@gmail.com>
2025-12-03 13:12:57 -0400

    Agreagmo carrito sesion

---
+++ script.js
@@ -0,0 +1,6 @@
+function scrollCarousel(amount) {
+    document.getElementById("carouselScroll").scrollLeft += amount;
+}
+document.getElementById("btnCarrito").addEventListener("click", function () {
+    window.location.href = "carrito.html";
+});
