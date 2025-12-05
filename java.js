427885eb32efc17f7c66784d6786e6b20c6f1bd8
jharlyno-alt <jharlyno@gmail.com>
2025-12-05 08:53:55 -0400

    Se Agrego las paginas de cocina, hogar, tecnologia y modifico la estructutade la pagina principal dejando solo los 4 productos principales

--- java.js
+++ java.js
@@ -39,3 +39,40 @@ document.getElementById("btn-buscar").addEventListener("click", function() {
         //  redirigir a otra página, filtrar productos
     }
 });
+
+
+function scrollCarousel(amount) {
+    document.getElementById("carouselScroll").scrollLeft += amount;
+}
+document.getElementById("btnCarrito").addEventListener("click", function () {
+    window.location.href = "carrito.html";
+});
+function scrollCarousel(amount) {
+    document.getElementById("carouselScroll").scrollLeft += amount;
+}
+
+
+ // FORMULARIO CONTACTO
+    const formLogin = document.getElementById("form-contacto");
+    if(form){
+        form.addEventListener("submit", function(e){
+            e.preventDefault();
+            const nombre = document.getElementById("nombre").value;
+            const email = document.getElementById("email").value;
+
+            if(nombre.trim() === "" || email.trim() === ""){
+                alert("Por favor completa todos los campos.");
+                return;
+            }
+
+            alert(`Gracias ${nombre}, tu correo (${email}) se registró correctamente.`);
+            form.reset();
+        });
+    }
+
+    const formSesion = document.getElementById('formRegistro');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); 
+        alert("¡Ya estás registrado!");
+    });
